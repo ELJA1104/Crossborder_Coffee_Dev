2f0d666007e775f2374a8f90d543235264d701c3
EthanLing <ethuntcore@gmail.com>
2026-08-09 22:19:42 +0800

    9/8/2026

--- Code/customer_spawn_area.gd
+++ Code/customer_spawn_area.gd
@@ -1,5 +1,5 @@
 extends Node2D
-@export var exit_point : Node2D
+@export var exit_area : Node2D
 var customer_scene
 var customer
 
@@ -7,7 +7,8 @@ var customer
 func _ready() -> void:
 	customer_scene = preload("res://Scenes/customers.tscn")
 	customer = customer_scene.instantiate()
-
+	print(exit_area)
+	var exit_point : Vector2 = exit_area.global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,5 +17,4 @@ func _process(delta: float) -> void:
 func _on_button_pressed() -> void:
 	get_tree().root.add_child(customer)
 	customer.global_position = self.global_position
-	var customer_speed = randf_range(1,2)
-	customer.linear_velocity = (exit_point.global_position - self.global_position) * customer_speed
+	#customer.target = exit_point
--- Code/customers.gd
+++ Code/customers.gd
@@ -1,1 +1,11 @@
-extends RigidBody2D
+extends CharacterBody2D
+var speed = 300.0
+
+var target: Vector2
+
+func _physics_process(delta: float) -> void:
+	if position.distance_to(target) > 5.0:
+		velocity = position.direction_to(target) * speed
+	else:
+		velocity = velocity       
+	move_and_slide()
