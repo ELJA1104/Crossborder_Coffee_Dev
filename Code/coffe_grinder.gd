f8c92aa1e841815305402731c90fc3c74d8fdd1a
10539ws <vernonheng2012@gmail.com>
2026-08-09 20:31:05 +0800

    a

--- Code/coffe_grinder.gd
+++ Code/coffe_grinder.gd
@@ -4,6 +4,8 @@ extends Node2D
 var time:int = 60
 #bool===========================================================================
 
+var done : bool = false
+var puck_entered :bool = false
 var grinder_closing_act : bool = true
 var coffee_entered :bool = false
 var two :bool = false
@@ -19,8 +21,6 @@ var puck
 @onready var label :Label = $Label
 @onready var grinder_open =$Grinder2
 @onready var grinder_close =$Grinder1
-@onready var Input_time = $input_time
-@onready var grinding = $grinding
 #func ==========================================================================
 
 func _ready() -> void:
@@ -46,7 +46,15 @@ func a():
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("coffee bean"):
 		coffee_entered = true
-
+		if grinder_closing_act ==true:
+			label.visible_characters = 0
+			label.text = "Please close the
+grinder"
+			for i in range(0,100):
+				label.visible_characters += 1
+				await get_tree().create_timer(0.05).timeout
+		else:
+			a()
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
@@ -59,45 +67,22 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 
 func _on_button_pressed() -> void:
 	if coffee_entered and two and can:
-		label.visible_characters = 0
-		label.text= "Please wait for 
-		"+ str(time)+ " seconds"
-		for i in range(0,29):
-			label.visible_characters += 1
-			await get_tree().create_timer(0.05).timeout
-		$grinding.start()
-		$input_time.start()
-
-
-func _on_grinding_timeout() -> void: #for grinding
-	label.visible_characters = 0
-	await get_tree().create_timer(0.5).timeout
-	label.text = "Congrat!"
-	for i in range(0,9):
-		label.visible_characters += 1
-		await get_tree().create_timer(0.05).timeout
-	grinding.stop()
-	only_congrat()
-	reset_two()
+		grinder_close = false
+		await get_tree().create_timer(0.1).timeout
+		only_congrat()
+		done = true
 
 
 func reset_two():
 	two = false
 
+
 func only_congrat():
-	label.text = "Congrat! "
-
-func _on_input_time_timeout() -> void: #for input_time
-	time -= 1
-	text_update()
-	if time == 0:
-		Input_time.stop()
-		time =60
-
-
-func text_update():
-	label.text= "Please wait for 
-	"+ str(time)+ " seconds"
+	label.visible_characters = 0
+	label.text = "Done。"
+	for i in range(0,10):
+		label.visible_characters += 1
+		await get_tree().create_timer(0.05).timeout
 
 
 func _on_closing_button_pressed() -> void:
@@ -112,12 +97,19 @@ func _on_opening_button_pressed() -> void:
 	if grinder_closing_act and coffee_entered:
 		two = true
 		can = true
+		label.visible_characters = 0
+		label.text = "Please press the
+ 'Grind' button"
+		for i in range(0,100):
+			label.visible_characters += 1
+			await get_tree().create_timer(0.05).timeout
 
 
 func _on_extract_area_body_entered(body:CharacterBody2D) -> void:
 	if body is Ethan_Puck:
 		puck = Ethan_Puck
-		_on_button_2_pressed()
+		puck_entered = true
+
 
 func _on_extract_area_body_exited(body:CharacterBody2D) -> void:
 	if body is Ethan_Puck:
@@ -125,7 +117,8 @@ func _on_extract_area_body_exited(body:CharacterBody2D) -> void:
 
 
 func _on_button_2_pressed() -> void:
-	if puck: 
-		puck.is_empty = false
-	else:
-		puck.is_empty = true
+	if puck_entered and done:
+		if puck: 
+			puck.is_empty = false
+		else:
+			puck.is_empty = true
