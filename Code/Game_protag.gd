c2d6a331fa790cb6c92edd844b0ddd8ce96ff5a2
10539ws <vernonheng2012@gmail.com>
2026-08-11 23:26:11 +0800

    new assignment

--- Code/Game_protag.gd
+++ Code/Game_protag.gd
@@ -1,25 +1,15 @@
 extends CharacterBody2D
-
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const Speed = 1500
+var health : int = 100
+var has_finished_drink : bool = false
+var is_at_counter : bool = false
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	movement()
 	move_and_slide()
+	
+func movement():
+	var direc = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = direc * Speed
+	
---
+++ Code/change_scene.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func up():
+	if Input.is_action_pressed("Up"):
+		pass
