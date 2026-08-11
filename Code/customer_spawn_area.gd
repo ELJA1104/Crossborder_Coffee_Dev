84a4d3f8507773be857161120a7e2550cf248202
EthanLing <ethuntcore@gmail.com>
2026-08-11 18:34:19 +0800

    I'm loosing hope

--- Code/customer_spawn_area.gd
+++ Code/customer_spawn_area.gd
@@ -1,20 +1,60 @@
 extends Node2D
 @export var exit_area : Node2D
+#@export var exit_point = exit_area.global_position
 var customer_scene
 var customer
+var ran_drink : int
+var flavour
+var ran_ice : int
+var temp
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	customer_scene = preload("res://Scenes/customers.tscn")
-	customer = customer_scene.instantiate()
 	print(exit_area)
-	var exit_point : Vector2 = exit_area.global_position
+	#print(exit_point)
+	drink_select()
+	drink_temp()
+	print(temp + flavour)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _on_button_pressed() -> void:
+func _on_button_pressed() -> void:	
+	customer = customer_scene.instantiate()
 	get_tree().root.add_child(customer)
 	customer.global_position = self.global_position
 	#customer.target = exit_point
+
+
+
+func drink_select():
+	ran_drink = randi_range(0, 6)
+	if ran_drink == 0:
+		flavour = "Water"
+	elif ran_drink == 1:
+		flavour = "Americano"
+	elif ran_drink == 2:
+		flavour = "Cappuccino"
+	elif ran_drink == 3:
+		flavour = "Espresso"
+	elif ran_drink == 4:
+		flavour = "Macchiato"
+	elif ran_drink == 5:
+		flavour = "Mocha"
+	elif ran_drink == 6:
+		flavour = "Latte"
+
+
+func drink_temp():
+	ran_ice = randi_range(0, 2)
+	if ran_ice == 0:
+		temp = "Hot "
+	elif ran_ice == 1:
+		temp = "Warm "
+	elif ran_ice == 2:
+		temp = "Cold "
+
+	if temp == "Cold " and flavour == "Water":
+		temp = "Cold  "
+	if temp == "Hot " and flavour == "Water":
+		temp = "Hot  "
--- Code/game_protag.gd
+++ Code/game_protag.gd
@@ -1,2 +1,15 @@
 extends CharacterBody2D
-const Speed = 300
+const Speed = 1500
+var health : int = 100
+var has_finished_drink : bool = false
+var is_at_counter : bool = false
+
+
+func _physics_process(delta: float) -> void:
+	movement()
+	move_and_slide()
+	
+func movement():
+	var direc = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = direc * Speed
+	
