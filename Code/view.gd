1f7008e934678ea495131825bf43510963a2e05c
10539ws <vernonheng2012@gmail.com>
2026-08-26 00:06:24 +0800

    a

--- Code/view.gd
+++ Code/view.gd
@@ -1,17 +1,13 @@
 extends CharacterBody2D
 
 @export var speed = 100
-@onready var left : Button = $lft/lft_but
-@onready var right :Button = $rght/rght_but
-@onready var limit : Camera2D = $"player's_view"
 @onready var view = self
 var cursor = load("res://Assets/PNG/just_dot.png")
 var cus = preload("res://Scenes/customer_spawn_area.tscn")
 
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(cursor)
-	left.hide()
-	right.hide()
+
 
 func _physics_process(delta: float) -> void:
 	movement_lerp(delta)
@@ -20,20 +16,3 @@ func movement_lerp(delta):
 	var dir = get_global_mouse_position()
 	var wishvel = dir * delta
 	view.global_position = lerp(view.global_position,wishvel,1)
-
-func _on_lft_mouse_entered() -> void:
-	left.show()
-
-func _on_lft_mouse_exited() -> void:
-	left.hide()
-
-func _on_rght_mouse_entered() -> void:
-	right.show()
-
-func _on_rght_mouse_exited() -> void:
-	right.hide()
-
-func tp():
-	var cus_2 = cus.instantiate()
-	if left.pressed:
-		view.position = cus_2
