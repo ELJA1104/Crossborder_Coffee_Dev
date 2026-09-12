cc5e81a418955052e35f2f44f529065fcbd2d4b3
ELJA1104 <ethuntcore@gmail.com>
2026-09-12 14:04:27 +0800

    12/09/2026
    added a proposed fix to a bug

--- Code/base_scene.gd
+++ Code/base_scene.gd
@@ -32,3 +32,7 @@ func _on_cinnamon_powder_pressed() -> void:
 	elif cinnamon_powder_pressed == true:
 		anim_plyr.play("Cinnamon Powder slide out")
 		cinnamon_powder_pressed = false
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	pass # Replace with function body.
