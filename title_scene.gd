a7e4ac6c8ad6e2898b036e9d6fa3330afef137ce
10539ws <vernonheng2012@gmail.com>
2026-09-18 01:05:39 +0800

    start functionable,start working setting

--- title_scene.gd
+++ title_scene.gd
@@ -3,23 +3,23 @@ extends Node2D
 @onready var quit = $quit
 @onready var nuhuh = $Label2
 @onready var rickroll = $Control/VideoStreamPlayer
-@onready var player = $AnimationPlayer
 
+
+var icon = 0
 var times = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	rickroll.modulate.a = 1
 	rickroll.stream = preload("res://Smartest Rick Roll but with a different link. ( 720 X 1280 ).ogv")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_quit_pressed() -> void:
-	if times == 2:
+	if times == 1:
 		nuhuh.text = "Fine"
-		nuhuh.modulate.a = 1
+		for i in range(10):
+			quit.modulate.a -= 0.1
+			await get_tree().create_timer(0.05).timeout
+		for i in range(10):
+			nuhuh.modulate.a += 0.1
 		await get_tree().create_timer(1).timeout
 		get_tree().quit()
 	for i in range(10):
@@ -40,7 +40,19 @@ func _on_quit_pressed() -> void:
 	quit.modulate.a = 1
 	times += 1
 
+func _on_start_pressed() -> void:
+	$ColorRect2.global_position = $ColorRect.global_position
+	await get_tree().create_timer(1).timeout
+	get_tree().change_scene_to_file("res://Scenes/change_scene.tscn")
+	
 
-
-func _on_setting_pressed() -> void:
-	player.play("new_animation")
+func _on_icon_pressed() -> void:
+	var lgth = $Sprite2D/Label.text.length()
+	icon += 1
+	if icon == 9:
+		$Sprite2D/Label.text = "Don't touch me"
+	$Sprite2D/Label.visible_characters = 0
+	for i in range(lgth):
+		$Sprite2D/Label.visible_characters += 1
+		await get_tree().create_timer(0.05).timeout
+	print(icon)
