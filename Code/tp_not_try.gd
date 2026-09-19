2c1783ecdf178594fabe618a2c2b545ea02a30e1
10539ws <vernonheng2012@gmail.com>
2026-09-19 14:48:19 +0800

    a

--- Code/tp_not_try.gd
+++ Code/tp_not_try.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 	Input.set_custom_mouse_cursor(cursor)
 
 func _physics_process(delta: float) -> void:
-	tp(delta)
+	tp()
 	space()
 
 func space():
@@ -26,12 +26,10 @@ func space():
 		if !toggle:
 			print("false")
 
-func tp(delta):
+func tp():
 	if !toggle:
 		dir = cus_alt.global_position
 		view.global_position = lerp(view.global_position, dir, 0.4)
 	if toggle:
 		dir = pvz_alt.global_position
 		view.global_position = lerp(view.global_position, dir, 0.4)
-	var wishvel = get_global_mouse_position()*delta
-	view.global_position = lerp(view.global_position, wishvel+dir, 0.4)
--- title_scene.gd
+++ title_scene.gd
@@ -6,22 +6,12 @@ extends Node2D
 
 
 var icon = 0
-var times = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	rickroll.modulate.a = 1
 	rickroll.stream = preload("res://Smartest Rick Roll but with a different link. ( 720 X 1280 ).ogv")
 
 func _on_quit_pressed() -> void:
-	if times == 1:
-		nuhuh.text = "Fine"
-		for i in range(10):
-			quit.modulate.a -= 0.1
-			await get_tree().create_timer(0.05).timeout
-		for i in range(10):
-			nuhuh.modulate.a += 0.1
-		await get_tree().create_timer(1).timeout
-		get_tree().quit()
 	for i in range(10):
 		quit.modulate.a -= 0.1
 		await get_tree().create_timer(0.05).timeout
@@ -36,9 +26,15 @@ func _on_quit_pressed() -> void:
 	await get_tree().create_timer(7).timeout
 	rickroll.stop()
 	rickroll.modulate.a = 0
-	nuhuh.modulate.a = 0
 	quit.modulate.a = 1
-	times += 1
+	nuhuh.text = "JK"
+	for i in range(10):
+		quit.modulate.a -= 0.1
+		await get_tree().create_timer(0.05).timeout
+	for i in range(10):
+		nuhuh.modulate.a += 0.1
+		await get_tree().create_timer(1).timeout
+		get_tree().quit()
 
 func _on_start_pressed() -> void:
 	$ColorRect2.global_position = $ColorRect.global_position
