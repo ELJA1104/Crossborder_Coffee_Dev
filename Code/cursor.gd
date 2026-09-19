3781399bf9aba14285d1df016feab24f347f8f35
Zion-coder-cloud <leezionguanhang0585@sbis.edu.my>
2026-09-19 13:51:00 +0800

    0.002
    Fixed the scaling for the zombies.
    Working on the background (Krita)

--- Code/cursor.gd
+++ Code/cursor.gd
@@ -7,7 +7,12 @@ func _physics_process(delta: float) -> void:
 	self.global_position = get_global_mouse_position()
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
+func _input(event):
+	if event.is_action_pressed("m1"):
+		_kill_enemy()
 
+func _kill_enemy():
+	queue_free()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	pass
--- Code/enemy.gd
+++ Code/enemy.gd
@@ -1,13 +1,21 @@
 class_name Enemy
 extends CharacterBody2D
 
-
 var target : Player
 
+@export var start_y: float = 0
+@export var max_forward_y: float = 650
+@export var min_scale: float = 1
+@export var max_scale: float = 8
 
-var SPEED = 100
+
+var SPEED = 75
+
 
 func _ready():
+	pass
+	
+func _set_multi_scale():
 	pass
 	
 	
@@ -17,8 +25,15 @@ func _physics_process(delta: float) -> void:
 		velocity = direction * SPEED
 	move_and_slide()
 	
+	global_position.y += 100 * delta
+	var total_distance = max_forward_y - start_y
+	var curr_distance = global_position.y - start_y
+	var travel_percentage = curr_distance / total_distance
+	var target_scale = lerp(min_scale, max_scale, travel_percentage)
+	var final_scale = clamp(target_scale, min_scale, max_scale)
 	
-
+	scale = Vector2(final_scale, final_scale)
+	
 func _kill_enemy():
 	queue_free()
 	
